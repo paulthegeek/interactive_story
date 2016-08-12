a2e49656bcb5a607925a392cb8a8d4a10351a3be
paulthegeek <pj2454@gmail.com>
2016-08-12 06:34:32 -0600

    Add content to pages, navigation through pages, and layout

--- interactive_story/ViewController.swift
+++ interactive_story/ViewController.swift
@@ -17,7 +17,14 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "startAdventure" {
+            if let pageController = segue.destinationViewController as? PageController {
+                pageController.page = Adventure.story
+            }
+        }
     }
 
 
